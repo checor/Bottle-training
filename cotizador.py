b7bd295590bd879ed93889f3acb25522fb30f6d1
Sergio Urbina <checor@gmail.com>
2015-03-09 16:56:28 -0600

    Update cotizador.py
    Agregado cotización del dolar

--- cotizador.py
+++ cotizador.py
@@ -2,6 +2,7 @@
 import csv, re
 from amazon.api import AmazonAPI
 from bottle import route, run, template, static_file, request
+from bs4 import BeautifulSoup as BS
 
 def get_csv(csv_file='rootkey.csv'):
     dic = {}
@@ -12,11 +13,28 @@ def get_csv(csv_file='rootkey.csv'):
             dic[s[0]] = s[1]
     return dic
 
+
+def get_dollar():
+    ban_url = "http://www.dolar.mx/precio-del-dolar-hoy/"
+    opener = MyOpener()
+    page = BS(opener.open(ban_url).read())
+    precios = page.findAll("h2")
+    #Esto puede ser altamente variable, no confiable
+    valores = str(precios[3])
+    precio = valores[42:47]
+    vprint("Precio del dolar: ", precio)
+    return float(precio)
+
 def get_price(url, store='Amazon'):
     if store == 'Amazon':
         dic = get_csv()
         api = AmazonAPI(dic['AWSAccessKeyId'], dic['AWSSecretKey'], dic['asoc'])
-        id_ = re.search("/dp/(.*?)/", url).group(1)
+        if url.search('/dp/') > 0:
+        	id_ = re.search("/dp/(.*?)/", url).group(1)
+        elif url.search('/product/') > 0:
+        	id_ = re.search("/product/(.*?)/", url).group(1)
+        else:
+        	return 0
         product = api.lookup(ItemId=id_)
         price = str(product.price_and_currency[0])
         price = float(price.translate(None, '$'))
@@ -30,7 +48,7 @@ def home():
                 print url
                 print "\n\n\n\n\n"
                 return template('cotizador.tpl', usd_price=get_price(url),
-                        usd_mxn=15.3)
+                        usd_mxn=get_dollar())
 	else:
 		return static_file('cock.html', root=".")
 
